5b05b013c778e297e7ffb0d3f24fccd4c798fe4c
ProPhone-Inc <dallas@prophone.io>
2025-03-05 12:09:49 +0000

    Testing stage auto-deploy

--- webhook-stage.js
+++ webhook-stage.js
@@ -5,27 +5,27 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
-const SECRET = "prophone2025"; // Replace with a random string
+const SECRET = "dashboardstage"; // Use a different secret for stage
 
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
     const sig = `sha256=${crypto.createHmac('sha256', SECRET).update(JSON.stringify(req.body)).digest('hex')}`;
-
     if (req.headers['x-hub-signature-256'] !== sig) {
         return res.status(401).send('Invalid signature');
     }
 
+    console.log("Received push to 'Stage' branch. Deploying Stage...");
     exec('/var/www/stage/deploy.sh', (error, stdout, stderr) => {
         if (error) {
             console.error(`Error: ${stderr}`);
-            return res.status(500).send('Webhook execution failed');
+            return res.status(500).send('Stage deployment failed');
         }
-        console.log(`Success: ${stdout}`);
-        res.send('Webhook executed successfully');
+        console.log(`Stage Deployment Success: ${stdout}`);
+        res.send('Stage deployment completed');
     });
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening for GitHub webhooks on port ${PORT}`);
+    console.log(`Listening for Stage GitHub webhooks on port ${PORT}`);
 });
