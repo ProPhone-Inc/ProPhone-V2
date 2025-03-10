bce161a63eaa24ca978fb3dfe08a86538141629f
ProPhone <dallas@thereynoldsgroup.net>
2025-03-10 14:32:04 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 cd /var/www/main
-git pull origin Main
+GIT_SSH_COMMAND='ssh -i ~/.ssh/github_deploy_key' git pull origin Main
 npm install
-pm2 restart vite-main
+pm2 restart vite-main --update-env
